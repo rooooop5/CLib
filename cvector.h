0b806a49c0c15c7f3c8b5483ef9f5124ec9db49b
rooooop5 <83465610+rooooop5@users.noreply.github.com>
2025-11-16 14:52:08 +0530

    Add files via upload

--- cvector.h
+++ cvector.h
@@ -8,13 +8,9 @@
         type* data; \
         size_t size; \
         size_t capacity;\
+        type front;\
+        type last;\
     }cvector_##type; \
-    static inline void init_##type(cvector_##type *v)\
-    { \
-        v->data=NULL;\
-        v->size=0;\
-        v->capacity=0;\
-    }\
     static inline void cvec_##type##_push(cvector_##type *v,type value)\
     {\
         if(v->size==v->capacity)\
@@ -22,19 +18,53 @@
             v->capacity=v->capacity?v->capacity*2:1;\
             v->data=realloc(v->data,v->capacity*sizeof(type));\
         }\
+        if(v->size==0)\
+        {\
+            v->front=value;\
+            v->last=value;\
+        }\
+        else\
+        {\
+            v->last=value;\
+        }\
         v->data[v->size++]=value;\
     }\
     static inline type cvec_##type##_pop(cvector_##type *v)\
     {\
+        v->last=v->data[(v->size)-2];\
         return v->data[--v->size];\
     }\
     static inline void cvec_##type##_remove(cvector_##type *v,int idx)\
     {\
-        for(int _i=idx;_i<v->size;_i++)\
+        for(int _i=idx;_i<(v->size)-1;_i++)\
         {\
-            v->data[idx]=v->data[idx+1];\
-        } \
-        (v->size)--;\
+            v->data[_i]=v->data[_i+1];\
+        }\
+        if(idx==0) v->front=v->data[0];\
+        v->size--;\
+    }\
+    static inline void cvec_##type##_insert(cvector_##type *v,type value,int idx,int copies)\
+    {\
+        if(!(((v->size)+copies)<=v->capacity))\
+        {\
+            v->capacity*=2;\
+            v->data=realloc(v->data,v->capacity*sizeof(type));\
+        }\
+        int str_cp=copies,str_idx=idx;\
+        v->size+=copies;\
+        while(copies>0)\
+        {\
+            for(int _i=v->size-1;_i>=idx;_i--)\
+            {\
+                v->data[_i+1]=v->data[_i];\
+            }\
+            idx++;\
+            copies--;\
+        }\
+        for(int _i=0;_i<str_cp;_i++)\
+        {\
+            v->data[str_idx++]=value;\
+        }\
     }
 
 #define iterate_byref(v,_i_te__rat_or,type)\
@@ -46,9 +76,22 @@
     for(size_t i=0;i<v.size;i++) \
         for(int  __o_n_c_e_=1; __o_n_c_e_; __o_n_c_e_=0)\
             for(type i_t__e__r_a_t_or=(v.data[i]);__o_n_c_e_;__o_n_c_e_=0)
-        
-#define init_cvec(type,name)\
-    cvector_##type name;\
-    init_##type(&name);
+
+#define cvec_print(v,fmt)\
+    do\
+    {\
+        printf("[");\
+        for(int i=0;i<v.size;i++)\
+        {\
+            printf(fmt,v.data[i]);\
+            if(i<v.size-1)\
+            {\
+                printf(",");\
+            }\
+        }\
+        printf("]\n");\
+    }while(0);
+
+#define init_cvector(type) (cvector_##type){.data=NULL,.size=0,.capacity=0} 
 
 #endif
